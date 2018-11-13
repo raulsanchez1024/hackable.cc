325a6cd57e21165751c707ac604da91920d09bfb
Raul <raulsanchez1024@gmail.com>
2018-11-13 15:26:01 -0700

    Updated domains 11/13/18

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -18,9 +18,9 @@ class App extends Component {
           </h1>
           <p>(we update our domains daily <span role="img" aria-label="confidence">😏</span>)</p>
 
-          <h3><a href="mailto:raulsanchez1024@gmail.com?subject=I would like to buy Allotra.com" className="domain">Allotra.com - $125</a></h3>
-          <h3><a href="mailto:raulsanchez1024@gmail.com?subject=I would like to buy Fivefy.com" className="domain">Fivefy.com - $45</a></h3>
-          <h3><a href="mailto:raulsanchez1024@gmail.com?subject=I would like to buy Privus.xyz" className="domain">Privus.xyz - $11</a></h3>
+          <h3><a href="mailto:mks148@gmail.com?subject=I would like to buy NewPlugins.com 🤓" className="domain">NewPlugins.com - $500</a></h3>
+          <h3><a href="https://www.namecheap.com/domains/registration/results.aspx?domain=kiwis.io" className="domain">Kiwis.io - $32.88</a></h3>
+          <h3><a href="https://www.namecheap.com/domains/registration/results.aspx?domain=fruit.li" className="domain">Fruit.li - $14.99</a></h3>
         </div>
 
         <Footer />
